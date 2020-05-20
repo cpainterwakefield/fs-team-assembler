1518bf8f1faf7f6f782ac1c356ef51e51cbcc9c2
Jesus Nunez <jnunez@mines.edu>
2020-05-20 13:15:24 -0600

    slight changes in server.js and the start of a database

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,2 +1,23 @@
-const dbConfig = require("../config/db.config.js");
+const dbConfig = require("../db.config.js");
 
+const Sequelize = require('sequelize');
+//Create instance of sequelize using config file
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorAliases: false,
+
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        aquire: dbConfig.pool.aquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
-
-app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
