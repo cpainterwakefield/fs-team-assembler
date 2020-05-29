3943c5a14b4a0f3cf843bfd2da2077366a7d932c
michaelhumphrey <michaelhumphrey@mymail.mines.edu>
2020-05-29 14:34:01 -0600

    starting axios

--- backend/create-db.sql
+++ backend/create-db.sql
@@ -1,11 +1,3 @@
-CREATE TABLE student(
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    username TEXT NOT NULL,
-    project_id INTEGER,
-    FOREIGN KEY (project_id) REFERENCES project(id)
-);
-
 CREATE TABLE advisor(
     id INTEGER PRIMARY KEY,
     name TEXT,
@@ -24,9 +16,16 @@ CREATE TABLE project(
     advisor_id INTEGER,
     client_id INTEGER,
     min_students INTEGER,
-    max_students INTEGER
+    max_students INTEGER,
     FOREIGN KEY (advisor_id) REFERENCES advisor(id),
     FOREIGN KEY (client_id) REFERENCES client(id)
+);
+
+CREATE TABLE student(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    username TEXT NOT NULL,
+    project_id INTEGER REFERENCES project(id)
 );
 
 CREATE TABLE prefer_project_xref(
@@ -42,7 +41,7 @@ CREATE TABLE prefer_teammate_xref(
     preferree_id INTEGER,
     PRIMARY KEY (preferrer_id, preferree_id),
     FOREIGN KEY (preferrer_id) REFERENCES student(id),
-    FOREIGN KEY (preferee_id) REFERENCES student(id)
+    FOREIGN KEY (preferree_id) REFERENCES student(id)
 );
 
 CREATE TABLE avoid_teammate_xref(
