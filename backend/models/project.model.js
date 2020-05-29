a976229a349888410c0e7f5c08ae92bc13fc7ec4
Jesus Nunez <jnunez@mines.edu>
2020-05-29 15:32:40 -0600

    fixed null?

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -4,37 +4,31 @@ module.exports = (sequelize, Sequelize) => {
     const Project = sequelize.define("project", {
         name: {
             type: Sequelize.STRING,
-            defaultValue: null,
             allowNull: true
         },
 
         description: {
             type: Sequelize.STRING,
-            defaultValue: null,
             allowNull: true
         },
 
         advisor_Id: {
             type: Sequelize.INTEGER,
-            defaultValue: null,
             allowNull: true
         },
 
         client_Id: {
             type: Sequelize.INTEGER,
-            defaultValue: null,
             allowNull: true
         },
 
         min_Students: {
             type: Sequelize.INTEGER,
-            defaultValue: null,
             allowNull: true
         },
 
         max_Students: {
             type: Sequelize.INTEGER,
-            defaultValue: null,
             allowNull: true
         }
     });
