ddc150c8005d3b6a175506bbcb2270a2c66ee5c7
Jesus Nunez <jnunez@mines.edu>
2020-06-03 16:29:57 -0600

    test 2

--- reconnect/src/views/student/Edit.vue
+++ reconnect/src/views/student/Edit.vue
@@ -83,8 +83,8 @@ export default {
   },
   mounted() {
     var self=this;
-    const requestStud = axios.get('http://localhost:8080/api/students');
-    const requestProj = axios.get('http://localhost:8080/api/projects');
+    const requestStud = axios.get('http://localhost:8080/api/students', {withCredentials: true});
+    const requestProj = axios.get('http://localhost:8080/api/projects', {withCredentials: true});
 
     axios.all([requestStud, requestProj]).then(axios.spread((...responses) => {
       const responseStud = responses[0]
@@ -99,7 +99,7 @@ export default {
     })
     // need to change
     let id = 1
-    axios.get('http://' + location.hostname + ':8080/api/students/' + id)
+    axios.get('http://' + location.hostname + ':8080/api/students/' + id, {withCredentials: true})
       .then(response => {
         console.log(response)
         // JSON responses are automatically parsed.
@@ -139,7 +139,7 @@ export default {
         third_project: this.thirdProj,
         selection_preference: this.preference
         
-      })
+      }, {withCredentials: true})
       .then(response => {
         console.log(response);
       })
