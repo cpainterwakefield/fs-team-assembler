f5347243ee5ec26584b051cdcd9e9bf20c3aeb63
michaelhumphrey <michaelhumphrey@mymail.mines.edu>
2020-06-09 13:10:11 -0600

    small home.vue change

--- reconnect/src/views/home.vue
+++ reconnect/src/views/home.vue
@@ -1,12 +1,16 @@
 <template>
   <center>
-    <div>
-      <h3> Welcome to CS@Mines Reconnect! This website asks you and your classmates for preferences about what team you want to work on, then sorts everyone into teams based on who you want to work with, who you want to avoid working with, whether you'd rather work with teammates or on
+    <div class = "message">
+      <div class = "main">
+        <h3> Welcome to CS@Mines Reconnect! This website asks you and your classmates for preferences about what team you want to work on, then sorts everyone into teams based on who you want to work with, who you want to avoid working with, whether you'd rather work with teammates or on
 a particular project, and, of course, the projects that you would like to work on.</h3>
+      </div>
       <hr>
-      <span> This website uses cookies for the purpose of user authentication and authorization. By clicking continue below, you consent to our use of cookies for this purpose.</span>
+      <div class="cookie">
+        <span > This website uses cookies for the purpose of user authentication and authorization. By clicking continue below, you consent to our use of cookies for this purpose.</span>
+      </div>
+      <v-btn class="primary" x-large color='primary' href="/login">Continue</v-btn>
     </div>
-    <v-btn class="primary" x-large color='primary' href="/login">Continue</v-btn>
   </center>
 </template>
 
@@ -28,4 +32,21 @@ export default {
 
 
 <style>
+  .message {
+    width: 70%;
+    border: 2px solid black;
+    border-radius: 10px;
+    background: #D3D3D3;
+  }
+
+  .main {
+    padding: 15px;
+    margin: 25px;
+  }
+
+  .cookie {
+    padding: 15px;
+    margin: 25px;
+  }
+
 </style>
